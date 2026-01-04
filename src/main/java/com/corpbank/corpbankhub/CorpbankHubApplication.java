da36f0e9c68015bb2e4f05e6dff698dbfe1764f2
DESKTOP-ERM8LSQ\TK COMPUTER <mahreenmehar202@gmail.com>
2026-01-04 10:50:55 +0500

    Initial Spring Boot setup with PostgreSQL and Flyway

---
+++ src/main/java/com/corpbank/corpbankhub/CorpbankHubApplication.java
@@ -0,0 +1,13 @@
+package com.corpbank.corpbankhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpbankHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorpbankHubApplication.class, args);
+    }
+
+}
